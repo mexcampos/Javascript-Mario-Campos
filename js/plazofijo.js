db96962e90edffb7561f445580bcef29d072a281
mexcampos <camposmario1996@gmail.com>
2023-06-03 11:17:30 -0300

    Tercera Pre-Entrega

---
+++ js/plazofijo.js
@@ -0,0 +1,60 @@
+// CALCULADORA DE PLAZO FIJO
+// Enviar el formulario
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", processForm);
+function processForm(event) {
+    event.preventDefault(); // Evita el envío del formulario
+    const section = document.getElementById("resultado");
+    section.scrollIntoView({ behavior: 'smooth' });
+
+// Evento para poder presionar enter
+let submitButton = document.querySelector('input[type="submit"]');
+submitButton.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); 
+    formulario.dispatchEvent(new Event('submit')); 
+  }
+});
+// Obtén los valores de los campos del formulario
+let capital = document.getElementById('capital').value;
+let meses = document.getElementById('meses').value;
+let tasa = document.getElementById('tasa').value;
+
+// Realiza las acciones deseadas con los valores capturados
+console.log('Capital inicial:', capital);
+console.log('Cantidad de meses:', meses);
+console.log('Tasa de interés:', tasa);
+
+// calculo la tasa de interés mensual
+let tasaMensual = parseFloat(tasa / 100 / 365 * 30);
+console.log(tasaMensual.toFixed(4));
+
+// funcion if porque la variable meses no puede ser inferior a 1
+if (meses < 1){
+    alert("La inversión no puede ser menor a 1 mes");
+} else {
+// Iteración para capitalizar mes a mes los intereses
+    for(let i = 1; i <= meses; i++){
+        capital = capital * (tasaMensual + 1);
+        console.log(capital.toLocaleString("es-AR", {style: "currency", currency: "ARS"}));
+}}
+document.getElementById("resultado").innerHTML = capital.toLocaleString("es-AR", {style: "currency", currency: "ARS"});
+}
+
+let cajaResultado = document.getElementById("cajaResultado");
+
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  ocultarForm();
+  mostrarResultado();
+});
+
+// muestra el resultado y oculta el formulario
+function ocultarForm() {
+  formulario.classList.add("oculto");
+}
+
+function mostrarResultado() {
+  cajaResultado.classList.remove("oculto");
+}
---
+++ js/testInversor.js
@@ -0,0 +1,108 @@
+// Obtener las respuestas almacenadas en localStorage (si existen)
+let respuestas = JSON.parse(localStorage.getItem('respuestas')) || [];
+
+// Obtener referencia al formulario
+let formulario = document.getElementById("formulario");
+
+// Agregar un controlador de eventos para el evento "submit"
+formulario.addEventListener("submit", processForm);
+
+// Función para procesar el formulario al enviarlo
+function processForm(event) {
+    event.preventDefault();
+
+    // Obtener las respuestas seleccionadas por el usuario
+    const conocimientoInput = document.querySelector('input[name="conocimiento"]:checked').value;
+    const experienciaInput = document.querySelector('input[name="experiencia"]:checked').value;
+    const ahorrosInput = document.querySelector('input[name="ahorros"]:checked').value;
+    const inversionInput = document.querySelector('input[name="inversion"]:checked').value;
+    const tiempoInput = document.querySelector('input[name="tiempo"]:checked').value;
+    const objetivoInput = document.querySelector('input[name="objetivo"]:checked').value;
+    const actitudInput = document.querySelector('input[name="actitud"]:checked').value;
+
+    // Agregar las respuestas al array
+    respuestas = [parseInt(conocimientoInput), parseInt(experienciaInput), parseInt(ahorrosInput), parseInt(inversionInput), parseInt(tiempoInput), parseInt(objetivoInput), parseInt(actitudInput)];
+
+    // Almacenar las respuestas en localStorage
+    localStorage.setItem('respuestas', JSON.stringify(respuestas));
+
+    // Función para cargar las respuestas almacenadas desde localStorage
+    function cargarRespuestas() {
+    const respuestasGuardadas = localStorage.getItem('respuestas');
+    if (respuestasGuardadas) {
+        respuestas = JSON.parse(respuestasGuardadas);
+    }}
+
+    // Llamar a la función para cargar las respuestas almacenadas al cargar la página
+    cargarRespuestas();
+
+    // Calcular la suma de las respuestas
+    const sum = respuestas.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+    console.log(sum);
+
+    // Objeto que contiene los datos sobre cada tipo de inversor
+    const perfiles = [
+        {
+            inversor: "conservador",
+            rentaVariable: 10,
+            rentaFija: 90
+        },
+        {
+            inversor: "conservador/moderado",
+            rentaVariable: 25,
+            rentaFija: 75
+        },
+        {
+            inversor: "moderado",
+            rentaVariable: 50,
+            rentaFija: 50
+        },
+        {
+            inversor: "moderado/arriesgado",
+            rentaVariable: 75,
+            rentaFija: 25
+        },
+        {
+            inversor: "arriesgado",
+            rentaVariable: 90,
+            rentaFija: 10
+        }
+    ];
+
+    // Función para mostrar el resultado y ocultar el formulario
+    function mensaje() {
+        let perfil;
+
+        if (sum >= 5 && sum <= 11) {
+            perfil = findPerfil("conservador");
+        } else if (sum > 11 && sum <= 17) {
+            perfil = findPerfil("conservador/moderado");
+        } else if (sum > 17 && sum <= 23) {
+            perfil = findPerfil("moderado");
+        } else if (sum > 23 && sum <= 29) {
+            perfil = findPerfil("moderado/arriesgado");
+        } else if (sum > 29 && sum <= 35) {
+            perfil = findPerfil("arriesgado");
+        } else {
+            alert("Debes ingresar respuestas válidas");
+            return;
+        }
+        console.log(perfil);
+
+        document.getElementById("resultado").innerHTML = `Eres un inversor ${perfil.inversor}.\nTu portfolio debería tener un ${perfil.rentaVariable}% de renta variable y un ${perfil.rentaFija}% de renta fija.`;
+    }
+
+    // Función para encontrar el perfil del inversor
+    function findPerfil(inversor) {
+        return perfiles.find(p => p.inversor === inversor);
+    }
+    mensaje(sum);
+    
+    
+    // Ocultar el formulario y mostrar el resultado
+    let cajaResultado = document.getElementById("cajaResultado");
+    cajaResultado.classList.remove("oculto");
+    formulario.classList.add("oculto");
+}
+
+
